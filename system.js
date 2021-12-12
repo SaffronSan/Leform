cc5808e467dbe002e88be0a0be4f35f9e8dcff02
SaffronSan <94629230+SaffronSan@users.noreply.github.com>
2021-12-12 16:07:39 -0500

    Update system.js

--- system.js
+++ system.js
@@ -2,7 +2,7 @@ function sysCheck(){
   let name = document.getElementById("uname").value;
   let pass = document.getElementById("upass").value;
   var text;
-  if(name == "gcell" && pass == "123"){
+  if(name == "gcell" || name == "anish" && pass == "123"){
       alert("Nice, you're good at math");
       window.open("https://saffronsan.github.io/Leform//rickroll.html","_blank");
    }else{
