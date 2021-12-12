bd0f02f01a0681299e7235a3f05247bfab4cc5a1
SaffronSan <94629230+SaffronSan@users.noreply.github.com>
2021-12-12 11:21:46 -0500

    Update system.js

--- system.js
+++ system.js
@@ -3,10 +3,10 @@ function sysCheck(){
   let pass = document.getElementById("upass").value;
   var text;
   if(name == "gcell" && pass == "123"){
-      alret("Nice, you're good at math");
+      alert("Nice, you're good at math");
       window.open("Saffronsan.github.io/homepage.html","_blank');
    }else{
-      alret("Noob, get outta here");
+      alert("Noob, get outta here");
     }
   
 }
